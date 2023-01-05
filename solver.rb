53a6e5e7670794b23800ca76518b97be0f2ffe6e
fayob <adedejifavour6@gmail.com>
2023-01-05 14:10:02 +0100

    fix rubocop error

--- solver.rb
+++ solver.rb
@@ -1,28 +1,30 @@
 class Solver
-  def initialize value
+  def initialize(value)
     @value = value
   end
 
-  def factorial (integer)
-    return "#{integer} is not a positive value" if integer < 0
-    return 1 if integer == 0
+  def factorial(integer)
+    return "#{integer} is not a positive value" if integer.negative?
+    return 1 if integer.zero?
+
     integer * factorial(integer - 1)
   end
 
-  def reverse word
-    @value.reverse(word.split('').reverse.join(''))
+  def reverse(word)
+    @value.reverse(word.chars.reverse.join)
   end
 
-  def fizzbuzz integer
-    return "Please provide a valid number" if !integer.is_a? Integer
-    if integer % 3 == 0 && integer % 5 == 0
-      "fizzbuzz"
-    elsif integer % 3 == 0
-      "fizz"
-    elsif integer % 5 == 0
-      "buzz"
+  def fizzbuzz(integer)
+    return 'Please provide a valid number' unless integer.is_a? Integer
+
+    if (integer % 3).zero? && (integer % 5).zero?
+      'fizzbuzz'
+    elsif (integer % 3).zero?
+      'fizz'
+    elsif (integer % 5).zero?
+      'buzz'
     else
-      integer.to_s 
+      integer.to_s
     end
   end
 end
--- solver_spec.rb
+++ solver_spec.rb
@@ -1,9 +1,9 @@
 require './solver'
 
 describe Solver do
-  describe "Test for factorial method" do
-    solver = Solver.new("qwert")
-    it "Should test for correct in factorial method in solver class" do
+  describe 'Test for factorial method' do
+    solver = Solver.new('qwert')
+    it 'Should test for correct in factorial method in solver class' do
       # mock = double("Solver")
       # expect(mock).to receive(:factorial).with(4)
 
@@ -13,38 +13,38 @@ describe Solver do
       expect(solver.factorial(5)).to eql(120)
     end
 
-    it "should test for error input in factorial method in solver class" do 
-      expect(solver.factorial(-1)).to eql("-1 is not a positive value")
-      expect(solver.factorial(-8)).to eql("-8 is not a positive value")
+    it 'should test for error input in factorial method in solver class' do
+      expect(solver.factorial(-1)).to eql('-1 is not a positive value')
+      expect(solver.factorial(-8)).to eql('-8 is not a positive value')
     end
   end
 
-  describe "Test for reverse method" do
-    it "Should reverse input passed into reverse method" do
-      mock = instance_double("Testing")
+  describe 'Test for reverse method' do
+    it 'Should reverse input passed into reverse method' do
+      mock = instance_double('Testing')
       expect(mock).to receive(:reverse).and_return('olleh')
 
-     solver = Solver.new(mock)
-     solver.reverse("hello")
+      solver = Solver.new(mock)
+      solver.reverse('hello')
     end
   end
 
-  describe "Test for fizzbuzz method" do
-    solver = Solver.new("Name")
-    it "Should return fizz for multiple of 3" do
-      expect(solver.fizzbuzz(3)).to eql("fizz")
+  describe 'Test for fizzbuzz method' do
+    solver = Solver.new('Name')
+    it 'Should return fizz for multiple of 3' do
+      expect(solver.fizzbuzz(3)).to eql('fizz')
     end
-    it "Should return buzz for multiple of 5" do
-      expect(solver.fizzbuzz(5)).to eql("buzz")
+    it 'Should return buzz for multiple of 5' do
+      expect(solver.fizzbuzz(5)).to eql('buzz')
     end
-    it "Should return fizzbuzz for multiple of 3 and 5" do
-      expect(solver.fizzbuzz(15)).to eql("fizzbuzz")
+    it 'Should return fizzbuzz for multiple of 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eql('fizzbuzz')
     end
-    it "Should return string of number if argument provided is not divisible by 3 or 5" do
-      expect(solver.fizzbuzz(7)).to eql("7")
+    it 'Should return string of number if argument provided is not divisible by 3 or 5' do
+      expect(solver.fizzbuzz(7)).to eql('7')
     end
-    it "Should return error message when a string argument is provided" do
-      expect(solver.fizzbuzz("the")).to eql("Please provide a valid number")
+    it 'Should return error message when a string argument is provided' do
+      expect(solver.fizzbuzz('the')).to eql('Please provide a valid number')
     end
   end
 end
